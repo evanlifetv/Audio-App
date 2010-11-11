f0d7769f60ab4a4be6f93fef8d37fb9083fbb32a
evanlifetv <evan.hamilton@lifechurch.tv>
2010-11-10 21:40:48 -0600

    new background!!

--- Classes/AppDelegate.m
+++ Classes/AppDelegate.m
@@ -40,7 +40,7 @@
 	
 	//initialize the audio controls view (top half of the screen)
 	self.controlsViewController = [[AudioControlsViewController sharedInstance] autorelease];
-	self.controlsViewController.view.frame = CGRectMake(0, 20, 768, 510);
+	self.controlsViewController.view.frame = CGRectMake(0, 20, 768, 533);
 	
 	//initialize each view controller that will be a tab in the tab bar controller
 	SweepGeneratorViewController *sweep = [[[SweepGeneratorViewController alloc] initWithNibName:@"SweepGeneratorViewController" bundle:nil] autorelease];
@@ -50,7 +50,7 @@
 	SupportViewController *support = [[[SupportViewController alloc] initWithNibName:@"SupportViewController" bundle:nil] autorelease];
 	
 	//set the tab bar controller's view to the bottom half of the screen
-	self.tabBarController.view.frame = CGRectMake(0, 530, 768, 494);
+	self.tabBarController.view.frame = CGRectMake(0, 530, 768, 471);
 	
 	//create an array out of the view controllers and give them to the tab bar controller
 	self.tabBarController.viewControllers = [NSArray arrayWithObjects:sweep, tone, pink, playlist, support, nil];

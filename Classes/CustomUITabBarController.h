ee5fa94c74c15136fa62912e23a09f1604939014
evanlifetv <evan.hamilton@lifechurch.tv>
2011-01-16 13:03:49 -0600

    Started laying out background image for TabBar based on this code:  http://duivesteyn.net/2010/01/16/iphone-custom-tabbar-background-image/

---
+++ Classes/CustomUITabBarController.h
@@ -0,0 +1,7 @@
+// CustomUITabBarController.h
+#import "UIKit/UIKit.h"
+@interface CustomUITabBarController: UITabBarController {
+    IBOutlet UITabBar *tabBar1;
+}
+@property(nonatomic, retain) UITabBar *tabBar1;
+@end
